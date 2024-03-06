aaa6d8f937e16c6f7d0ffff38fe3bae0467a5dcb
Aravind-Kumar-2000 <aravindmpkas@gmail.com>
2024-03-06 17:06:50 +0530

    cookies

--- index.js
+++ index.js
@@ -4,11 +4,31 @@ const { userRoutes } = require("./Routes/userRoutes"); //Importing userRoutes mi
 
 const app = express(); //Initiating express
 
+const cookieParser = require("cookie-parser"); //Importing cookieParser middleware
+
 app.use(express.json()); //To get the data from client side in a json format
 
 const PORT = 7500;
 
-app.use(userRoutes); //Assigning the userRoutes middleware by using app.use()
+app.use(userRoutes); //Initiating the userRoutes middleware by using app.use()
+
+app.use(cookieParser("Secret")); //Initiating the cookieParser middleware by using app.use(), this cookieParser middleware always accepts a secret message.
+
+
+//Example to see how cookieParser works
+app.get("/api/message", (request, response)=>{
+  console.log(request.cookies); //To see the cookies coming from client side in the terminal
+  
+  console.log(request.headers.cookie); //To see the cookie header coming from client side in the terminal
+
+  response.cookie("hello", "world", {maxAge:60000, signed:true})
+
+  if(response.cookie.hello && response.cookie.hello === "world"){
+    return response.status(200).send({msg:"Hello"})
+  } else{
+    return response.status(400).send("Bad Request!")
+  }
+})
 
 //To start a Server
 app.listen(PORT, (err) => {
